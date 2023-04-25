67f625555b095bd4f1ea09981c69a422158228df
licong <1411197585@qq.com>
2023-04-26 00:16:01 +0800

    feat: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
+import VueMacros from 'unplugin-vue-macros'
+import { checker } from 'vite-plugin-checker'
 
 export default defineConfig({
   resolve: {
@@ -15,8 +16,9 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
+  base: '/resume/',
   plugins: [
-    VueMacros({
+    VueMacros.vite({
       plugins: {
         vue: Vue({
           reactivityTransform: true,
@@ -50,6 +52,8 @@ export default defineConfig({
     // https://github.com/antfu/unocss
     // see uno.config.ts for config
     UnoCSS(),
+
+    checker({ typescript: true }),
   ],
 
   // https://github.com/vitest-dev/vitest
